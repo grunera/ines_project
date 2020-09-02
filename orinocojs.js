2783c4c8cb9dcc0d7e487d75c2113683a5cc27b7
Audrey Gruner <66530333+grunera@users.noreply.github.com>
2020-09-03 00:21:38 +0200

    FIRST COMMIT

---
+++ orinocojs.js
@@ -0,0 +1,10 @@
+// Affichage du code de quantité
+document.getElementById("quantity").addEventListener("change", function (e) {
+    console.log("Code de quantity : " + e.target.value);
+});
+
+
+// Affichage du code couleur choisi
+document.getElementById("color").addEventListener("change", function (e) {
+    console.log("Code de color : " + e.target.value);
+});
